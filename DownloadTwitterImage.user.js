52aa30d4d05651a0e1dcef7f7c0e5de6293b5383
87 <crycli87+git@gmail.com>
2016-05-04 12:53:32 +0900

    consoleに原寸画像のUrlを吐くところまで

--- DownloadTwitterImage.user.js
+++ DownloadTwitterImage.user.js
@@ -5,5 +5,17 @@
 この下のimgタグが本体.
 (ちなみにgifは動画扱いのよう)
 */
+(function main() {
+var tweets = $(".tweet:has(.AdaptiveMedia-photoContainer)");
+$.each(tweets, addButton);
+})();
 
-$()
+function addButton(index, tweet){
+  var originUrl = getOriginUrl(tweet);
+  // TODO: ボタン追加
+}
+
+function getOriginUrl(tweet){
+  var imgUrl = $(tweet).find(".AdaptiveMedia-photoContainer > img").attr("src");
+  return imgUrl + ":orig";
+}
