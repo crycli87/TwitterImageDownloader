f7cd30a34ab126397e4a48657bf58ee1531b8bbb
87 <crycli87+git@gmail.com>
2016-06-01 18:01:39 +0900

    複数ファイルDLに対応 ほぼ完成

--- DownloadTwitterImage.user.js
+++ DownloadTwitterImage.user.js
@@ -1,23 +1,47 @@
-// TODO:複数ファイル対応. 多分aタグのdownloadではダメなのでonClickでやる.
-// TODO:ファイル名を.jpg-origからちゃんとしたのに直す
+// TODO: 複数ファイルDL時になんか許可が必要なの治す
 var buttonStyle = "";
-var buttonElement = '<div class="ProfileTweet-action Dl-Image"><a download href=""><input type="button" value="画像をDL" style="width: 70px;"></a></div>"';
+var buttonElement = '<div class="ProfileTweet-action Dl-Image"><input type="button" value="画像をDL" style="width: 70px;"></div>"';
 
 (function main() {
 var tweets = $(".tweet:has(.AdaptiveMedia-photoContainer)");
+// TODO: 動的に読み込まれた奴にボタン適応できてないようす.
 $.each(tweets, addButton);
 })();
 
 function addButton(index, tweet){
-  var originUrl = getOriginUrl(tweet);
-  var fileName = getFileName(originUrl);
+  var originUrls = getOriginUrls(tweet);
   $(tweet).find("div.ProfileTweet-actionList").append(buttonElement);
-  $(".Dl-Image").find("a").attr("href":originUrl);
+  $(tweet).find("input").bind("click", {urls:originUrls}, download);
 }
 
-function getOriginUrl(tweet){
-  var imgUrl = $(tweet).find(".AdaptiveMedia-photoContainer > img").attr("src");
-  return imgUrl + ":orig";
+function download(event){
+  var urls = event.data.urls;
+  urls.forEach(function(url,i,ar){
+    var request = new XMLHttpRequest();
+    var fileName = getFileName(url);
+    request.open("GET", url, true);
+    request.responseType = "blob";
+    request.onload = function (e) {
+      var blob = request.response;
+      var objectUrl = window.URL.createObjectURL(blob);
+      var link = document.createElement("a");
+      document.body.appendChild(link);
+      link.href = objectUrl;
+      link.download = fileName;
+      link.click();
+      document.body.removeChild(link);
+    };
+    request.send();
+  });
+}
+
+function getOriginUrls(tweet){
+  var imgs = $(tweet).find(".AdaptiveMedia-photoContainer > img");
+  var imgUrls = [];
+  $(imgs).each(function(i,img){
+    imgUrls.push($(img).attr("src") + ":orig");
+  });
+  return imgUrls;
 }
 
 function getFileName(url){
